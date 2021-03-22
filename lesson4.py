f48de0a8840fb03f92a43c1a83ac5de4e17914ac
Maria <tsareva@vniitek.ru>
2021-03-22 16:19:01 +0300

    Первая версия

---
+++ lesson4.py
@@ -0,0 +1,19 @@
+# t = int(input('Введите количество отработанных сотрудником часов'))
+# s = int(input('Введите почасовую ставку'))
+# b = int(input('Введите размер премии'))
+#
+# def my_func(t, s, b):
+#     return (t * s) + b
+#
+# print(my_func(t, s, b))
+from typing import Any, Union
+
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+print('Список', my_list)
+for n in my_list:
+    if not next(n) >= n
+        print(next(n))
+    else:
+        pass
+# new_gen_list = [n for n in my_list if n > n-1]
+# print(f'Новый список', new_gen_list)
